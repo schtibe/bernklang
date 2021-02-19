ca057b536be99fe137ee9c26ad62a4acc84ad93a
Stefan Heinemann <stefan.heinemann@codedump.ch>
2021-02-19 17:29:18 +0100

    Style header, add more info

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import "ol/ol.css";
 	import Pin from "./Pin.svelte";
+	import Header from "./Header.svelte";
 	import { onMount, setContext } from "svelte";
 
 	import Map from "ol/Map";
@@ -40,37 +41,19 @@
 	});
 </script>
 
-<style>
-	#map {
-		width: 100vw;
-		height: 100vh;
-	}
-
-	#title {
-		position: fixed;
-		z-index: 10;
-		top: 0;
-		left: calc(50% - 200px);
-		width: 400px;
-		background: rgba(255, 255, 255, 0.6);
-		text-align: center;
-		border-radius: 5px;
-	}
-
-	h1 {
-		padding: 0rem 0.1rem;
-		line-height: 0.5rem;
-	}
-</style>
-
 <main>
 	<div id="map" />
-	<div id="title">
-		<h1>Bernklang</h1>
-	</div>
+	<Header />
 	<div>
 		{#each pinData as data}
 			<Pin {data} />
 		{/each}
 	</div>
 </main>
+
+<style>
+	#map {
+		width: 100vw;
+		height: 100vh;
+	}
+</style>
---
+++ src/Header.svelte
@@ -0,0 +1,110 @@
+<script>
+  let isOpen = false;
+
+  import InfoIcon from "./InfoIcon.svelte";
+
+  function toggleDrawer() {
+    isOpen = !isOpen;
+  }
+</script>
+
+<div>
+  <div id="title">
+    <button on:click={toggleDrawer}>
+      <div>
+        <div class="space">&nbsp;</div>
+        <h1>Bernklang</h1>
+        <div class="info-icon">
+          <InfoIcon />
+        </div>
+      </div>
+    </button>
+
+    <main class="info" class:is-open={isOpen}>
+      <div>The sounds of Berne, Switzerland</div>
+
+      <div class="contrib">by Stefan Heinemann</div>
+    </main>
+  </div>
+</div>
+
+<style>
+  #title {
+    position: fixed;
+    z-index: 10;
+    top: 0;
+    left: calc(50% - 200px);
+    width: 400px;
+    background: rgba(255, 255, 255, 0.6);
+    text-align: center;
+    border-radius: 5px;
+  }
+
+  .info {
+    display: none;
+    margin-top: 3rem;
+    font-size: 1.5rem;
+    padding-bottom: 3rem;
+    opacity: 0;
+    transform: opacity 1s ease-out;
+  }
+
+  .is-open {
+    display: flex;
+    flex-direction: column;
+    opacity: 1;
+  }
+
+  .contrib {
+    margin-top: 1.5rem;
+    font-size: 1rem;
+  }
+
+  .space {
+    width: 1.2em;
+  }
+
+  h1 {
+    padding: 0rem 0.1rem;
+    line-height: 0.5rem;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  button > div {
+    display: flex;
+    justify-items: center;
+    align-items: baseline;
+  }
+
+  button:hover {
+    color: rgb(95, 95, 95);
+  }
+
+  .info-icon {
+    fill: currentColor;
+    opacity: 0.5;
+    padding-left: 1rem;
+  }
+
+  @media screen and (max-width: 390px) {
+    h1 {
+      font-size: 2rem;
+    }
+
+    #title {
+      width: 200px;
+      left: calc(50% - 100px);
+    }
+
+    .info {
+      font-size: 1rem;
+    }
+
+    .contrib {
+      font-size: 0.8rem;
+    }
+  }
+</style>
