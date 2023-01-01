8f98b58d979075ba8e1a1ba80c67e572b9d160c8
Stefan Heinemann <stefan.heinemann@codedump.ch>
2023-01-01 18:08:01 +0100

    Restrict zooming out

--- src/App.svelte
+++ src/App.svelte
@@ -1,59 +1,60 @@
 <script>
-	import "ol/ol.css";
-	import Pin from "./Pin.svelte";
-	import Header from "./Header.svelte";
-	import { onMount, setContext } from "svelte";
+  import "ol/ol.css";
+  import Pin from "./Pin.svelte";
+  import Header from "./Header.svelte";
+  import { onMount, setContext } from "svelte";
 
-	import Map from "ol/Map";
-	import View from "ol/View";
-	import TileLayer from "ol/layer/Tile";
-	import OSM from "ol/source/OSM";
-	import { fromLonLat } from "ol/proj";
+  import Map from "ol/Map";
+  import View from "ol/View";
+  import TileLayer from "ol/layer/Tile";
+  import OSM from "ol/source/OSM";
+  import { fromLonLat } from "ol/proj";
 
-	import pinData from "./data.json";
+  import pinData from "./data.json";
 
-	let map;
+  let map;
 
-	const createMap = () => {
-		map = new Map({
-			target: "map",
-			layers: [
-				new TileLayer({
-					source: new OSM(),
-				}),
-			],
-			view: new View({
-				center: fromLonLat([7.4331, 46.9428]),
-				zoom: 14,
-			}),
-		});
-	};
+  const createMap = () => {
+    map = new Map({
+      target: "map",
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: fromLonLat([7.4331, 46.9428]),
+        zoom: 14,
+        minZoom: 14,
+      }),
+    });
+  };
 
-	const addPins = () => {};
+  const addPins = () => {};
 
-	onMount(() => {
-		createMap();
-		addPins();
-	});
+  onMount(() => {
+    createMap();
+    addPins();
+  });
 
-	setContext("map", {
-		getMap: () => map,
-	});
+  setContext("map", {
+    getMap: () => map,
+  });
 </script>
 
 <main>
-	<div id="map" />
-	<Header />
-	<div>
-		{#each pinData as data}
-			<Pin {data} />
-		{/each}
-	</div>
+  <div id="map" />
+  <Header />
+  <div>
+    {#each pinData as data}
+      <Pin {data} />
+    {/each}
+  </div>
 </main>
 
 <style>
-	#map {
-		width: 100vw;
-		height: 100vh;
-	}
+  #map {
+    width: 100vw;
+    height: 100vh;
+  }
 </style>
